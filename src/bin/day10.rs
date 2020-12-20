2a88f560c4cf36b332da5a6b0ef86f401458885d
David Wagner <wagdav@gmail.com>
2020-12-20 21:28:59 +0100

    Solve day10 part2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -1,7 +1,8 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fs::read_to_string;
 
-fn solve_part1(input: &[i32]) -> Option<usize> {
+fn solve_part1(input: &[i32]) -> usize {
     let mut x: Vec<i32> = input.to_vec();
     x.push(0);
 
@@ -20,34 +21,32 @@ fn solve_part1(input: &[i32]) -> Option<usize> {
         .filter(|&x| x == 3)
         .count();
 
-    Some(ones * (three + 1))
+    ones * (three + 1)
 }
 
-fn solve_part2(input: &[i32]) -> Option<i32> {
-    let mut x: Vec<i32> = input.to_vec();
-    x.push(0);
+fn solve_part2(input: &[i32]) -> usize {
+    let mut x = input.to_vec();
+    let mut m: HashMap<i32, usize> = HashMap::new();
 
     x.sort();
-    let diff: Vec<i32> = x.iter().zip(x.iter().skip(1)).map(|(a, b)| b - a).collect();
 
-    let mut res: i32 = 1;
-    let mut acc: Vec<i32> = vec![];
-    for d in diff {
-        if d == 1 {
-            acc.push(d);
-        } else {
-            if acc.len() > 1 {
-                let base: i32 = 2;
-                //let n_one_one = std::cmp::max(acc.len() as i32, 3);
-                res *= base.pow(acc.len() as u32 - 1);
-            }
-            acc.clear();
-        }
+    let last = x.pop().unwrap();
+    x.push(last);
+    x.push(last + 3);
+
+    m.insert(0, 1);
+    for i in &x {
+        m.insert(
+            *i,
+            *m.get(&(i - 1)).unwrap_or(&0)
+                + *m.get(&(i - 2)).unwrap_or(&0)
+                + *m.get(&(i - 3)).unwrap_or(&0),
+        );
     }
 
-    dbg!(res);
+    let last = x.iter().last().unwrap();
 
-    Some(res)
+    *m.get(last).unwrap()
 }
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
@@ -72,12 +71,12 @@ mod tests {
 
     #[test]
     fn example1() {
-        assert_eq!(solve_part1(&EXAMPLE1), Some(7 * 5));
-        //assert_eq!(solve_part2(&EXAMPLE1), Some(8));
+        assert_eq!(solve_part1(&EXAMPLE1), 7 * 5);
+        assert_eq!(solve_part2(&EXAMPLE1), 8);
     }
 
     #[test]
     fn example2() {
-        //assert_eq!(solve_part2(&EXAMPLE2), Some(19208));
+        assert_eq!(solve_part2(&EXAMPLE2), 19208);
     }
 }
